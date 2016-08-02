1eafa661d8b55e41b026f514657cca318147552d
akhlei <angusleikh@gmail.com>
2016-08-02 14:32:27 -0700

    previous version had module errors, reupload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # node_scraper
+<<<<<<< HEAD
+Terminal web scraper made with node.js 
+=======
 web scraping with node.js
+>>>>>>> 0edaf82a12ed67a13d155c9e6491a1996062623e
---
+++ index.js
@@ -0,0 +1,39 @@
+var repl = require('repl');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var url = 'http://substack.net/images/';
+
+
+request.get(url, function(error, response, html){
+  if(!error && response.statusCode === 200){
+    var $ = cheerio.load(html);
+    var finalArray = [];
+
+    $('tr').filter(function(){
+      var permit = $(this).children().first().children().text();
+      var extension = $(this).children().last().children().attr("href");
+      suffix = $(this).children().last().children().text().split('.')[1];
+
+      var csvString = permit + "," + (url + extension) + "," + ("." + suffix);
+      finalArray.push(csvString);
+    }
+
+    fs.writeFile('./scraped.csv', finalArray.join("\n"), 'utf8', function(err){
+      if (err){
+        console.log("Error occured");
+      } else{
+        console.log("Success!");
+      }
+    });
+  }
+});
+
+
+
+
+
+
+
+
+

0e5432ae2ade2e5ff561ff45d33ae35c33551d96
akhlei <angusleikh@gmail.com>
2016-08-02 15:14:40 -0700

    Lists whether a extension is actually a directory

--- index.js
+++ index.js
@@ -7,30 +7,38 @@ var url = 'http://substack.net/images/';
 
 request.get(url, function(error, response, html){
   if(!error && response.statusCode === 200){
-    var $ = cheerio.load(html);
-    var finalArray = [];
-
-    $('tr').filter(function(){
-      permit = $(this).children().first().children().text();
-      extension = $(this).children().last().children().attr("href");
-      suffix = $(this).children().last().children().text().split('.')[1];
-
-      var csvString = permit + "," + (url + extension) + "," + ("." + suffix);
-      finalArray.push(csvString);
-    });
-
-    fs.writeFile('./scraped.csv', finalArray.join("\n"), 'utf8', function(err){
-      if (err){
-        console.log("Error occured");
-      } else{
-        console.log("Success!");
-      }
-    });
+    $ = cheerio.load(html);
+    finalArray = [];
+    formatStrings();
+    writeToCSV();
   }
 });
 
 
+function writeToCSV()  {
+    fs.writeFile('./scraped.csv', finalArray.join("\n"), 'utf8', function(err){
+    if (err){
+      console.log("Error occured");
+    } else{
+      console.log("Success!");
+    }
+  });
+}
 
+function formatStrings() {
+    $('tr').filter(function(){
+    permit = $(this).children().first().children().text();
+    urlPath = $(this).children().last().children().attr("href");    // refactor so that instead of undefined, says directory
+    if ($(this).children().last().children().text().split('.')[1]===undefined){
+      extension = "directory";
+    }else {
+      extension = "." + $(this).children().last().children().text().split('.')[1];
+    };
+
+    var csvString = permit + "," + (url + urlPath) + "," + extension;
+    finalArray.push(csvString);
+  });
+}
 
 
 
